8b0eeb1f7ae0766d22f1fb8ddcc0dae4aa1e9bd6
Yashvanth-R <yashvanth1ga20ad059@gmail.com>
2025-09-21 16:35:56 +0530

    Add: Coupon validation function

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
 import { connectDB } from './lib/db.js';
-import cartRoutes from './routes/cart.route.js'
+import cartRoutes from './routes/cart.route.js';
+import couponRoutes from './routes/coupon.route.js';
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(cookieParser()); // Middleware to parse cookies
 app.use("/api/auth", authRoutes)
 app.use("/api/products", productRoutes)
 app.use("/api/cart", cartRoutes)
+app.use("/api/coupons", couponRoutes)
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT);
