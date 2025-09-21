55791aee8ff2fd416f31359b75d31caa4cc0af16
Yashvanth-R <yashvanth1ga20ad059@gmail.com>
2025-09-21 18:23:47 +0530

    Add: Payment schema for the DB

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRoutes from './routes/product.route.js';
 import { connectDB } from './lib/db.js';
 import cartRoutes from './routes/cart.route.js';
 import couponRoutes from './routes/coupon.route.js';
+import paymentRoutes from './routes/payment.route.js';
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/products", productRoutes)
 app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes)
+app.use("/api/payments", paymentRoutes)
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT);
