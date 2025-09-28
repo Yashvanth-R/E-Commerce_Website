0f0b51f9d9dab453c4d04c3eef43c4c0b2b36589
Yashvanth-R <yashvanth1ga20ad059@gmail.com>
2025-09-28 13:45:44 +0530

    Add: Create the routing pages for login and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+
 function App() {
 
   return (
-    <div className="text-red-500 text-5xl">
-      Hello Vite + React!
+    <div>
+      <Routes>
+        <Route path="/" element={< HomePage/>} />
+        <Route path="/signup" element={< SignUpPage/>} />
+        <Route path="/login" element={< LoginPage/>} />
+      </Routes>
+
     </div>
   )
 }
